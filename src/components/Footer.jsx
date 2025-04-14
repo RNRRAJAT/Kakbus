8b8a9dd1364a48f99ef4223ee30885a124e3310b
Rajat Roy <rajatroy977@gmail.com>
2025-04-14 22:41:12 +0530

    added download kakfirmware

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,25 +3,27 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className={`absolute flex justify-between text-white  w-full h-[30vh] bg-zinc-800 top-[300vh] `}>
-        <div className="left ml-10 pt-8">
+      <div className="left ml-10 pt-8">
 
         <div className="text-white">KAKBUS &copy; 2025</div>
         <div className=''>All rights reserved</div>
+      </div>
+      <div className="middle pt-6 flex flex-col gap-7">
+        <div>CONTACT US</div>
+        <div>
+
+          <div>+91 - 8910364902</div>
+          <div>terawattkakbus@gmail.com</div>
+          
+
         </div>
-        <div className="middle pt-6 flex flex-col gap-7">
-            <div>CONTACT US</div>
-            <div>
-                
-            <div>+91 - 8910364902</div>
-            <div>terawattkakbus@gmail.com</div>
-            </div>
-            
-        </div>
-        <div className="right flex flex-col gap-3 pt-6">
-            <a target='blank' href="https://www.youtube.com/@Terabus-offc"><div className="social flex mr-14 gap-4"><img className='h-8 rounded-md' src="\youtubeLogo.png" alt="" /><div>YOUTUBE</div></div></a>
-            <a target='blank' href="https://www.instagram.com/terrabus369/?utm_source=qr&igsh=MTV0Z25kN3ZoeHY2NQ%3D%3D#"><div className="social flex gap-4"><img className='h-8 rounded-md' src="\instagramLogo.jpeg" alt="" /><div>INSTAGRAM</div></div></a>
-        </div>
-      
+
+      </div>
+      <div className="right flex flex-col gap-3 pt-6">
+        <a target='blank' href="https://www.youtube.com/@Terabus-offc"><div className="social flex mr-14 gap-4"><img className='h-8 rounded-md' src="\youtubeLogo.png" alt="" /><div>YOUTUBE</div></div></a>
+        <a target='blank' href="https://www.instagram.com/terrabus369/?utm_source=qr&igsh=MTV0Z25kN3ZoeHY2NQ%3D%3D#"><div className="social flex gap-4"><img className='h-8 rounded-md' src="\instagramLogo.jpeg" alt="" /><div>INSTAGRAM</div></div></a>
+      </div>
+
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,13 @@ const Home = () => {
         <>
             <div className="pt-20 homeContainer absolute flex justify-around w-[100vw] h-[100vh] bg-cover bg-[url('/background5.jpg')]" >
 
-                <div className="homeright mt-8 ml-10">
+                <div className="homeright mt-8 ml-10 flex flex-col gap-5 items-center">
                     <img className='h-[65vh] rounded-md' src="\greatkakbus.png" alt="" />
+                    <a href="\downloads\Kakfirmware.zip" download>
+                        <div className="bg-blue-600 text-white px-6 py-3 rounded-md text-center w-fit cursor-pointer hover:bg-blue-700 transition">
+                             Download KAKFIRMWARE
+                        </div>
+                    </a>
 
                 </div>
                 <div className='homeLeft mt-32 mr-10 text-white flex flex-col items-center gap-6'>
