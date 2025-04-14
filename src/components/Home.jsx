59dbaef32956c778e9396dbe8a6ebf7c7ce77bda
Rajat Roy <rajatroy977@gmail.com>
2025-04-14 22:01:56 +0530

    changes in Homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,16 +7,16 @@ const Home = () => {
             <div className="pt-20 homeContainer absolute flex justify-around w-[100vw] h-[100vh] bg-cover bg-[url('/background5.jpg')]" >
 
                 <div className="homeright mt-8 ml-10">
-                    <img className='h-[65vh] rounded-md' src="\homeImage2.jpg" alt="" />
+                    <img className='h-[65vh] rounded-md' src="\greatkakbus.png" alt="" />
 
                 </div>
-                <div className='homeLeft mt-12 mr-10 text-white flex flex-col items-center gap-6'>
-                    <h2 className='text-5xl font-bold'>WELCOME</h2>
-                    <h3 className='text-2xl'>to</h3>
+                <div className='homeLeft mt-32 mr-10 text-white flex flex-col items-center gap-6'>
+                    {/* <h2 className='text-5xl font-bold'>WELCOME</h2>
+                    <h3 className='text-2xl'>to</h3> */}
                     <h1 className='text-8xl font-extrabold'>KAKBUS</h1>
                     <div className='flex flex-col items-center'>
-                        <h2 className='text-2xl'>Where innovation meets</h2>
-                        <h1 className='text-3xl'>Ideas</h1>
+                        <h2 className='text-2xl'>we don't dream</h2>
+                        {/* <h1 className='text-3xl'>Ideas</h1> */}
                     </div>
                     <Link to='/projects'><div className="explore text-3xl border rounded-md p-6 bg-zinc-950 cursor-pointer mt-10">EXPLORE OUR PROJECTS</div></Link>
                 </div>
